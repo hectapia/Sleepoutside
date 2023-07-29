ab989c24540891072abf5c89a7cd345a6c433d26
hectapia <oli21007@byui.edu>
2023-07-28 20:11:20 -0600

    Update hot--team2

--- src/js/product.js
+++ src/js/product.js
@@ -1,3 +1,10 @@
+import { getParam } from "./utils.mjs";
+import productDetails from "./productDetails.mjs";
+
+const productId = getParam("product");
+productDetails(productId);
+
+/*
 import { setLocalStorage } from "./utils.mjs";
 import { findProductById } from "./productData.mjs";
 
@@ -14,3 +21,4 @@ async function addToCartHandler(e) {
 document
   .getElementById("addToCart")
   .addEventListener("click", addToCartHandler);
+ */ 
