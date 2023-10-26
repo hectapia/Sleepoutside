d5dd5a1bd204f990574c518e6cb01ff58d399ce9
Hector Olivares Tapia <97076427+hectapia@users.noreply.github.com>
2023-10-25 23:02:35 -0600

    Update productData.mjs

--- src/js/productData.mjs
+++ src/js/productData.mjs
@@ -1,4 +1,3 @@
-const baseURL = import.meta.env.VITE_SERVER_URL;
 function convertToJson(res) {
   if (res.ok) {
     return res.json();
@@ -7,14 +6,13 @@ function convertToJson(res) {
   }
 }
 
-export async function getData(category) {
-  const response = await fetch(baseURL + `products/search/${category}`);
-  const data = await convertToJson(response);
-  return data.Result;
+export function getData(category = "tents") {
+  return fetch(`../json/${category}.json`)
+    .then(convertToJson)
+    .then((data) => data);
 }
 
 export async function findProductById(id) {
-  const response = await fetch(baseURL + `product/${id}`);
-  const product = await convertToJson(response);
-  return product.Result;
+  const products = await getData();
+  return products.find((item) => item.Id === id);
 }
