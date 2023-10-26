28435b39890c7bb12853e63c60e982f96b8ab27d
hectapia <oli21007@byui.edu>
2023-10-25 23:37:29 -0600

    restore

--- src/js/product-list.js
+++
@@ -1,6 +0,0 @@
-import productList from "./productList.mjs";
-import { getParam, loadHeaderFooter } from "./utils.mjs";
-
-loadHeaderFooter();
-const category = getParam("category");
-productList(".product-list", category);
